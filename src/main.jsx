b81c3233ebaf7749ffe8b3a5fe57e5910ba44eba
Divyesh195 <divyeshparmar0909@gmail.com>
2025-03-09 19:29:12 +0530

    Error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Navbar from './Navbar.jsx'
-import Weather from './weather.jsx'
+import Weather from './Weather.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
